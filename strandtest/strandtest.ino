5f8278bec89e90ca889a43d03721ad8b2d88b675
Paul Rudolph <paul.e.rudolph@gmail.com>
2018-12-18 09:33:03 -0800

    Adding more projects

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -1,224 +1,125 @@
-
 #include <Adafruit_NeoPixel.h>
 
-//Accelerometer Pins
-const int x = A0; // X pin connected to Analog 3
-const int y = A1; // Y pin connected to Analog 4
-const int z = A2; // Z pin connected to Analog 5
 
-const int ledPin = 8; // Z pin connected to Analog 5
+#define PIN 4
 
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, PIN, NEO_GRB + NEO_KHZ800);
 
-int tolerance=40; // Sensitivity of the movement
-boolean calibrated=false; // When accelerometer is calibrated - changes to true 
-boolean moveDetected=false; // When motion is detected - changes to true
-boolean breakingDetected=false;
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
 
+void setup() {
 
-//Accelerometer limits
-int xMin; //Minimum x Value
-int xMax; //Maximum x Value
-int xVal; //Current x Value
-
-int yMin; //Minimum y Value
-int yMax; //Maximum y Value
-int yVal; //Current y Value
-
-int zMin; //Minimum z Value
-int zMax; //Maximum z Value
-int zVal; //Current z Value
-
-unsigned long movedStartTime;
-
-unsigned long movementTimeout = 10000;
-
- Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, ledPin, NEO_GRB + NEO_KHZ800);
-void setup(){
- //Begin Serial communication
- Serial.begin(9600);
-
-  calibrateAccel();
- strip.begin();
+  strip.begin();
   strip.show(); // Initialize all pixels to 'off'
- 
 }
 
+void loop() {
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  // Send a theater pixel chase in...
+  theaterChase(strip.Color(127, 127, 127), 50); // White
+  theaterChase(strip.Color(127,   0,   0), 50); // Red
+  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
 
-
-void loop(){
-    Serial.println();
-   Serial.print(moveDetected);
- // Once the accelerometer is calibrated - check for movement 
- if(calibrated){
-   if(checkMotion()){
-     moveDetected=true;
-     movedStartTime= millis();
-   } 
- }
- 
-  if( (millis() - movedStartTime)>movementTimeout ){
-    moveDetected=false;
-  }
-
-
-if(moveDetected){
-  
-  frontLights();
-  rearLights(breakingDetected);
-  strip.setBrightness(50);
-  strip.show();
-}else{
-  strip.setBrightness(0);
-  strip.show();
-}
- 
- 
- delay(100);
- }
- 
-
-
-
-
-
-// Function used to calibrate the Accelerometer
-void calibrateAccel(){
- // reset alarm
- moveDetected=false;
- 
- //initialise x,y,z variables
- xVal = analogRead(x);
- xMin = xVal;
- xMax = xVal;
- 
- yVal = analogRead(y);
- yMin = yVal;
- yMax = yVal;
- 
- zVal = analogRead(z);
- zMin = zVal;
- zMax = zVal;
- 
-
- 
- //calibrate the Accelerometer (should take about 0.5 seconds)
- for (int i=0; i<50; i++){
- // Calibrate X Values
- xVal = analogRead(x);
- if(xVal>xMax){
- xMax=xVal;
- }else if (xVal < xMin){
- xMin=xVal;
- }
-
- // Calibrate Y Values
- yVal = analogRead(y);
- if(yVal>yMax){
- yMax=yVal;
- }else if (yVal < yMin){
- yMin=yVal;
- }
-
- // Calibrate Z Values
- zVal = analogRead(z);
- if(zVal>zMax){
- zMax=zVal;
- }else if (zVal < zMin){
- zMin=zVal;
- }
-
- //Delay 10msec between readings
- delay(10);
- }
- 
- //End of calibration sequence sound. ARMED.
- printValues(); //Only useful when connected to computer- using serial monitor.
- calibrated=true;
- 
+  rainbow(20);
+  rainbowCycle(20);
+  theaterChaseRainbow(50);
 }
 
-
-
-//Function used to detect motion. Tolerance variable adjusts the sensitivity of movement detected.
-boolean checkMotion(){
- boolean tempB=false;
- xVal = analogRead(x);
- yVal = analogRead(y);
- zVal = analogRead(z);
- 
- if(xVal >(xMax+tolerance)||xVal < (xMin-tolerance)){
- tempB=true;
- Serial.print("X  Moved ");
- Serial.println(xVal);
- }
- 
- if(yVal >(yMax+tolerance)||yVal < (yMin-tolerance)){
- tempB=true;
- Serial.print("Y Moved = ");
- Serial.println(yVal);
- }
- 
- if(zVal >(zMax+tolerance)||zVal < (zMin-tolerance)){
- tempB=true;
- 
- 
- if(zVal < (zMin-tolerance)){
-   breakingDetected=true;
- }
- 
- Serial.print("Z Moved = ");
- Serial.println(zVal);
- }else{
-  breakingDetected=false;
- }
- 
- return tempB;
-}
- 
-
-
-
-
-// Prints the Sensor limits identified during Accelerometer calibration.
-// Prints to the Serial monitor.
-void printValues(){
- Serial.print("xMin=");
- Serial.print(xMin);
- Serial.print(", xMax=");
- Serial.print(xMax);
- Serial.println();
- 
- Serial.print("yMin=");
- Serial.print(yMin);
- Serial.print(", yMax=");
- Serial.print(yMax);
- Serial.println();
- 
- Serial.print("zMin=");
- Serial.print(zMin);
- Serial.print(", zMax=");
- Serial.print(zMax);
- Serial.println();
- 
- Serial.println("------------------------");
-}
-
-
-void frontLights(){
-  for(int i =0;i<6;i++){
-   strip.setPixelColor(i, 255, 255, 255);
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
   }
 }
 
-void rearLights(boolean breaking){
-   if(breaking){
-     for(int i =6;i<12;i++){
-       strip.setPixelColor(i, 255, 0, 255);
-      }
-   }else{
-    for(int i =6;i<12;i++){
-     strip.setPixelColor(i, 255, 0, 0);
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
     }
-   }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+     
+      delay(wait);
+     
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+        }
+        strip.show();
+       
+        delay(wait);
+       
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
 }
 
